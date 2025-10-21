6c6c52433a2aef3a443445b79b2f08ea3cfae816
Adex324 <o.joeade@gmail.com>
2025-10-21 10:47:38 +0100

    2nd commit for oladex app

--- src/App.jsx
+++ src/App.jsx
@@ -95,8 +95,8 @@ function App() {
         </div>
         <div className="gallery-grid">
           {[
-            work1,
             work2,
+            work1,
             work3,
             work4,
             work5,
